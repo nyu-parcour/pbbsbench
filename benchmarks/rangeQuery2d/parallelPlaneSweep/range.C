368c5f251677aff721adcc0bddd7213edea24661
Darshan D <darshand2000@gmail.com>
2025-12-03 07:04:05 -0500

    Add augment around reduce map

--- benchmarks/rangeQuery2d/parallelPlaneSweep/range.C
+++ benchmarks/rangeQuery2d/parallelPlaneSweep/range.C
@@ -69,8 +69,9 @@ long range(Points const &points, Queries const &queries, bool verbose) {
   parlay::internal::timer t("range", verbose);
   RangeQuery r(points);
   t.next("build");
+  long total;
   auto ret1 = parlay::augment([&]() {
-  long total = parlay::reduce(parlay::map(queries, [&] (query q) {
+  total = parlay::reduce(parlay::map(queries, [&] (query q) {
   	          return (long) r.count_in_range(q);}));
   });
   /*auto result_s = parlay::map(queries, [&] (query q) {
